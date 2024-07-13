952e42f82e7bb79013c9a2975e91047e87b9d2a2
unknown <fmmolinero@gmail.com>
2024-07-13 15:30:24 -0300

    adding redux for project

--- frontend/store/App.tsx
+++ frontend/store/App.tsx
@@ -1,31 +1,37 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ProductDetailsScreen from '@/components/ProductDetails';
+import { Provider } from 'react-redux';
 import ProductListingScreen from '@/components/ProductList';
+import ProductDetailsScreen from '@/components/ProductDetails';
+import store from './redux/store';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
 
-        <Stack.Screen
-          name="Home"
-          component={ProductListingScreen}
-        />
+          <Stack.Screen
+            name="Home"
+            component={ProductListingScreen}
+          />
 
-        <Stack.Screen
-          name="ProductDetails"
-          component={ProductDetailsScreen}
-          options={{
-            headerTitleAlign: 'center',
-            title: 'Detail',
-          }}
-        />
+          <Stack.Screen
+            name="ProductDetails"
+            component={ProductDetailsScreen}
+            options={{
+              headerTitleAlign: 'center',
+              title: 'Detail',
+            }}
+          />
 
-      </Stack.Navigator>
-    </NavigationContainer>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+
+
   );
 }
---
+++ frontend/store/redux/actions/index.js
@@ -0,0 +1,9 @@
+// Aquí definirías tus acciones de Redux
+export const increment = () => ({
+    type: 'INCREMENT'
+  });
+  
+  export const decrement = () => ({
+    type: 'DECREMENT'
+  });
+  
