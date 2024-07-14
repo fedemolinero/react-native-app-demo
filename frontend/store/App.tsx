c1b654a8733f5438f3440d778b0aa8b565be1287
unknown <fmmolinero@gmail.com>
2024-07-14 11:29:14 -0300

    removing redux

--- frontend/store/App.tsx
+++ frontend/store/App.tsx
@@ -1,27 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Provider } from 'react-redux';
 import ProductListingScreen from '@/screens/ProductList';
 import ProductDetailsScreen from '@/screens/ProductDetails';
-import store from './redux/store';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={ProductListingScreen}
-          />
-          <Stack.Screen
-            name="ProductDetails"
-            component={ProductDetailsScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={ProductListingScreen}
+        />
+        <Stack.Screen
+          name="ProductDetails"
+          component={ProductDetailsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
